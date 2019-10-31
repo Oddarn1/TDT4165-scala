2f7c4835aea615c79e1eaf7f7ce9fe0598c8a460
Odd Andre Owren <oddaow@stud.ntnu.no>
2019-10-31 12:21:12 +0100

    Task2 is believed to be done

--- src/main/scala/task2/Main.scala
+++ src/main/scala/task2/Main.scala
@@ -33,21 +33,20 @@ object Main {
   //Use safe=false for not-threadsafe counter.
 
   /*With standard threaded counter, there might be concurrency problems, where the print is executed before
-  * one or both counters has been ran. This is a race condition, and is a problem when executing
+  * one or both counters has been started. This is known as a race condition, and is a problem when executing
   * multiple operations at once where you have no way to tell which is executed first, and the order is important.*/
   def ThreadedCounter(safe:Boolean):Unit={
     val p=thread(PrintCounter(safe))
-    val c1=if(safe){thread(ThreadSafeIncreaseCounter())}else{thread(IncreaseCounter())}
-    val c2=if(safe){thread(ThreadSafeIncreaseCounter())}else{thread(IncreaseCounter())}
+    val c1=if(safe) thread(ThreadSafeIncreaseCounter()) else thread(IncreaseCounter())
+    val c2=if(safe) thread(ThreadSafeIncreaseCounter()) else thread(IncreaseCounter())
     c1.start()
-    //Thread.sleep to provoke a race condition
     c2.start()
     p.start()
   }
 
   //Task 2c
   var safeCounter=0
-  def ThreadSafeIncreaseCounter():Unit=this.synchronized{safeCounter+=1}
+  def ThreadSafeIncreaseCounter():Unit=this.synchronized(safeCounter+=1)
 
   //Task2d
   /*Deadlock can happen when two threads are waiting for resources from each other, but this resource is locked
@@ -61,13 +60,15 @@ object Main {
   }
 
   def main(args: Array[String]): Unit = {
-    print("Threaded counter(try running a couple times):")
+    print("Disclaimer: Wanted results may not always happen. Threaded counter and DeadlockExample may give different" +
+      " results on different runs. \nPlease try again if wanted result is not present.\n\n")
+    print("Threaded counter:")
     ThreadedCounter(safe=false)
     Thread.sleep(500)
     print("\nThreadsafe counter:")
     ThreadedCounter(safe=true)
     Thread.sleep(500)
-    print("\nIf nothing happens for 10 seconds, there is a deadlock, program will crash. If no deadlock, try again.")
+    print("\nIf nothing happens for 10 seconds, there is a deadlock, program will crash.")
     DeadLockExample()
   }
 }
