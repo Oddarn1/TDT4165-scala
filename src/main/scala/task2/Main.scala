6e36fda1a9563a2244e50463b23a2c03396a4f7f
Odd Andre Owren <oddaow@stud.ntnu.no>
2019-10-31 12:04:33 +0100

    Not done

--- src/main/scala/task2/Main.scala
+++ src/main/scala/task2/Main.scala
@@ -22,41 +22,52 @@ object Main {
     }
   }
 
-  //Dummy-function for testing thread-function
-  def sum(x1:Int,x2:Int):Unit=print(x1+x2)
-
   private var counter:Int=0
+
   def IncreaseCounter():Unit=counter+=1
 
   //Task 2b
-  def PrintCounter():Unit=print(counter)
+  def PrintCounter(safe:Boolean):Unit=print(if(safe)safeCounter else counter)
 
   //Task 2b
-  //Remove comments for thread-safe version
-  def ThreadedCounter():Unit={
-    val p=thread(PrintCounter())
-    val c1=thread(/*ThreadSafe*/IncreaseCounter())
-    val c2=thread(/*ThreadSafe*/IncreaseCounter())
-    p.start()
+  //Use safe=false for not-threadsafe counter.
+
+  /*With standard threaded counter, there might be concurrency problems, where the print is executed before
+  * one or both counters has been ran. This is a race condition, and is a problem when executing
+  * multiple operations at once where you have no way to tell which is executed first, and the order is important.*/
+  def ThreadedCounter(safe:Boolean):Unit={
+    val p=thread(PrintCounter(safe))
+    val c1=if(safe){thread(ThreadSafeIncreaseCounter())}else{thread(IncreaseCounter())}
+    val c2=if(safe){thread(ThreadSafeIncreaseCounter())}else{thread(IncreaseCounter())}
     c1.start()
+    //Thread.sleep to provoke a race condition
     c2.start()
+    p.start()
   }
 
   //Task 2c
-  def ThreadSafeIncreaseCounter():Unit=this.synchronized{counter+=1}
+  var safeCounter=0
+  def ThreadSafeIncreaseCounter():Unit=this.synchronized{safeCounter+=1}
 
   //Task2d
   /*Deadlock can happen when two threads are waiting for resources from each other, but this resource is locked
   from access when a thread is using one resource.*/
 
-  //Function will not always cause deadlock, but in some cases. If nothing happens for 15 seconds, we have a deadlock
-  //The program crashes if deadlock is held for 15 seconds.
+  //Function will not always cause deadlock, but in some cases. If nothing happens for 10 seconds, we have a deadlock
+  //The program crashes if deadlock is held for 10 seconds.
   def DeadLockExample():Unit={
     val deadlock=Future.sequence(Seq(Future {Dead.y} , Future {Lock.x}))
-    Await.result(deadlock,Duration(15,"seconds"))
+    Await.result(deadlock,Duration(10,"seconds"))
   }
 
   def main(args: Array[String]): Unit = {
+    print("Threaded counter(try running a couple times):")
+    ThreadedCounter(safe=false)
+    Thread.sleep(500)
+    print("\nThreadsafe counter:")
+    ThreadedCounter(safe=true)
+    Thread.sleep(500)
+    print("\nIf nothing happens for 10 seconds, there is a deadlock, program will crash. If no deadlock, try again.")
     DeadLockExample()
   }
 }
