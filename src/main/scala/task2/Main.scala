ff162e063b2c3a88025805ef40c4000436944a71
David Aanestad <davidaan@stud.ntnu.no>
2019-11-01 14:15:58 +0100

    Task 2 polishing

--- src/main/scala/task2/Main.scala
+++ src/main/scala/task2/Main.scala
@@ -37,26 +37,52 @@ object Main {
     val p=thread(PrintCounter())
     val c1=thread(/*ThreadSafe*/IncreaseCounter())
     val c2=thread(/*ThreadSafe*/IncreaseCounter())
-    p.start()
     c1.start()
     c2.start()
+
+    c1.join()
+    c2.join()
+
+    p.start()
   }
+
+  /*
+    Upon running this code we will most of the time get 2. But on rare occasions, we will get 1.
+    Since c1 and c2 are run concurrently one of the counters might read the counter-value before the other one
+    is finished, and thus they end up writing the same value.
+    This can be problematic in any situation where different threads does operations on the same variable. In this
+    case, a counter.
+  */
 
   //Task 2c
   def ThreadSafeIncreaseCounter():Unit=this.synchronized{counter+=1}
 
-  //Task2d
-  /*Deadlock can happen when two threads are waiting for resources from each other, but this resource is locked
-  from access when a thread is using one resource.*/
+  /*
+    Task 2d
 
-  //Function will not always cause deadlock, but in some cases. If nothing happens for 15 seconds, we have a deadlock
-  //The program crashes if deadlock is held for 15 seconds.
+    Deadlocks can happen whenever two threads are waiting for resources from each other, but this resource is locked
+    from access when a thread is using one resource.
+
+    The function below will not always cause deadlock, only in some cases. If nothing happens for 15 seconds, we have
+    a deadlock. The program crashes if a deadlock is held for 15 seconds.
+
+    To prevent a deadlock we can use atomic execution so that the critical region is not parallel. We can also
+    make sure that locks happen a prioritized order, such that you ensure that any threads won't be waiting for others.
+    E.g. instead of two threads accessing elements in the following order
+    1 2
+    2 1
+    we can ensure that they instead will access them in an order that won't create any conflicts, like this:
+    1 2
+    1 2
+   */
+
   def DeadLockExample():Unit={
     val deadlock=Future.sequence(Seq(Future {Dead.y} , Future {Lock.x}))
     Await.result(deadlock,Duration(15,"seconds"))
   }
 
   def main(args: Array[String]): Unit = {
-    DeadLockExample()
+    ThreadedCounter()
+    //DeadLockExample()
   }
 }
